11a6639aaab9bd6c0c2b21f71774a66b2a384f36
Alex Meusburger <rpgmem@gmail.com>
2026-01-30 20:15:06 -0300

    Initial commit

---
+++ assets/js/tmq-admin.js
@@ -0,0 +1,31 @@
+( function ( $, tmq ) {
+	"use strict";
+
+	const $doc = $( document );
+	const { restUrl, restNonce } = tmq;
+
+	// select/deselect all table rows
+	$doc.on( "click", '.tmq-select-all', function () {
+		const checked = this.checked;
+		$( 'table[class*="total-mail-queue_page"] input[name="id[]"],.tmq-select-all' ).each( function () {
+			this.checked = checked;
+		});
+	});
+
+	// dynamically load message when opening a details element for the first time
+	$doc.on( "click", '[data-tmq-list-message-toggle]',function () {
+		const $btn = $( this );
+		const id = $btn.attr( "data-tmq-list-message-toggle" );
+		$btn.attr( "data-tmq-list-message-toggle", null );
+		$.get( `${restUrl}tmq/v1/message/${id}`, { _wpnonce: restNonce } ).always( function ( response, status ) {
+			if ( status === "success" && response.status === "ok" ) {
+				$( '[data-tmq-list-message-content]', $btn.closest( 'details' ) ).html( response.data.html );
+			} else {
+				const responseData = response.responseJSON || response.data;
+				console.log( responseData );
+				alert( "There was an error loading the message." );
+			}
+		});
+	});
+
+}) ( jQuery, tmq );
