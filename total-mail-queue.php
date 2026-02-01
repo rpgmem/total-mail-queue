e69aee24d52d2ee83b56989088661566b8c0570f
Alex Meusburger <rpgmem@gmail.com>
2026-02-01 18:29:04 -0300

    Update total-mail-queue.php

--- total-mail-queue.php
+++ total-mail-queue.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< Updated upstream
 /**
  * Plugin Name:       Total Mail Queue
  * Plugin URI:
@@ -15,9 +16,26 @@
  *
  * This plugin is a fork of Mail Queue by WDM (https://www.webdesign-muenchen.de)
  * Original plugin: https://wordpress.org/plugins/mail-queue/
+=======
+/*
+ Plugin Name:         Total Mail Queue
+ Plugin URI:          https://github.com/rpgmem/total-mail-queue
+ Description:         Take Control and improve Security of wp_mail(). Queue and log outgoing emails, and get alerted, if your website wants to send more emails than usual.
+ Version:             2.2.0
+ Requires at least:   5.9
+ Requires PHP:        7.4
+ Author:              Alex Meusburger
+ Author URI:          https://github.com/rpgmem
+ License:             GPLv3 or later
+ License URI:         https://www.gnu.org/licenses/gpl-3.0.html
+ Text Domain:         total-mail-queue
+ 
+ This plugin is a fork of Mail Queue by WDM (https://www.webdesign-muenchen.de)
+ Original plugin: https://wordpress.org/plugins/mail-queue/
+>>>>>>> Stashed changes
  */
 
-if (!defined('ABSPATH')) { exit; }
+ if (!defined('ABSPATH')) { exit; }
 
 
 /* ***************************************************************
